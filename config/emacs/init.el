dd438e874ac84d11e2f5ba16726a2c13e3de8011
d <>
2023-01-29 10:54:59 -0500

    Delete customizations from custom.el

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -217,17 +217,3 @@
 
 (use-package visual-fill-column
   :hook (org-mode . df/org-mode-visual-fill))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(visual-fill-column forge exec-path-from-shell magit counsel-projectile projectile hydra evil-collection evil general all-the-icons doom-themes helpful counsel ivy-rich which-key rainbow-delimiters doom-modeline ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
